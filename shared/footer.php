c388edeb639bd34187402e7a3f064a41a0effbf3
Dan Denney <tecdan7@yahoo.com>
2012-06-22 00:02:49 -0400

    Digging the link backgrounds

--- shared/footer.php
+++ shared/footer.php
@@ -1,4 +1,4 @@
 <footer>
 	<nav><a href="/">Home</a> <a href="/posts">Posts</a>
-	&copy; Dan Denney - I'm on the Twitters as <a href="http://twitter.com/dandenney">@dandenney</a>
+	<small>&copy; Dan Denney - I'm on the Twitters as <a href="http://twitter.com/dandenney">@dandenney</a></small>
 </footer>
