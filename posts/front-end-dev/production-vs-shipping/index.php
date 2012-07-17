3ff66bdcb054964eb971b4c469fb1fdfd19bf2d9
Dan Denney <tecdan7@yahoo.com>
2012-07-17 12:35:00 -0400

    title fix

--- posts/front-end-dev/production-vs-shipping/index.php
+++ posts/front-end-dev/production-vs-shipping/index.php
@@ -1,5 +1,5 @@
 <?php include "../../../shared/page-open-inception-cant-go-more-than-3-levels-deep.php" ?>
-	<title>Sass + Compass Pairing with Michael Parenteau</title>
+	<title>Production vs. Shipping</title>
 </head>  
 
 <body>
